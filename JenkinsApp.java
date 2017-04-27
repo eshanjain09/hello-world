b3ccb997ae6b9d3c6d965ec6d62f040c5c42be6c
ejain <eshanamarjain@gmail.com>
2017-04-27 18:23:48 +0530

    Update JenkinsApp.java

--- JenkinsApp.java
+++ JenkinsApp.java
@@ -2,7 +2,7 @@
 public class JenkinsApp {
 
 	public static void main(String[] args) {
-		System.out.println("JenkinsApp says \"Hello World!\"");
+		System.out.println("JenkinsApp says \"Hello World!\", This is version-2");
 
 	}
 
